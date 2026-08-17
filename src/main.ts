316137dc0a3ff27baf03bfd86210e216943037c6
taijulsir <princetaijul123@gmail.com>
2026-08-17 15:13:24 +0600

    feat: add swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,6 +16,17 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Multi-Vendor E-Commerce API')
+    .setDescription('Backend API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    useGlobalPrefix: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
